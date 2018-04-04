03e47bcf45178d608717014edfcd6d9ba3aab853
max <wangshuo216@outlook.com>
2018-04-04 20:40:59 +0800

    update task 38

--- 038.py
+++ 038.py
@@ -34,8 +34,46 @@ class Solution:
             result = list(set(result))
             result.sort()
         return result
+
+    def CubicSum(self, num_array):
+        num_array = [str(num) for num in num_array]
+        num_str = ''.join(num_array)
+        totalPerms = self.Permutation(num_str)
+        for perm in totalPerms:
+            sum1 = int(perm[0]) + int(perm[1]) + int(perm[2]) + int(perm[3])
+            sum2 = int(perm[4]) + int(perm[5]) + int(perm[6]) + int(perm[7])
+            sum3 = int(perm[0]) + int(perm[2]) + int(perm[4]) + int(perm[6])
+            sum4 = int(perm[1]) + int(perm[3]) + int(perm[5]) + int(perm[7])
+            sum5 = int(perm[0]) + int(perm[1]) + int(perm[4]) + int(perm[5])
+            sum6 = int(perm[2]) + int(perm[3]) + int(perm[6]) + int(perm[7])
+            if sum1 == sum2 and sum3 == sum4 and sum5 == sum6:
+                return True
+        return False
+
+
+    def Queen(self, num_array):
+        num_array = [str(num) for num in num_array]
+        num_str = ''.join(num_array)
+        totalPerms = self.Permutation(num_str)
+        def Judge(posList):
+            length = len(posList)
+            for i in range(length):
+                for j in range(length):
+                    if i == j:
+                        continue
+                    if i - j == int(posList[i]) - int(posList[j]) or j - i == int(posList[i]) - int(posList[j]):
+                        return False
+            return True
+        
+        for posList in totalPerms:
+            isQueen = Judge(posList)
+            if isQueen:
+                print(posList)
+
             
 # test
 s = Solution()
 print(s.Permutation('abc'))
 print(s.Combination('abc'))
+print(s.CubicSum([1, 1, 1, 1, 1, 1, 1, 1]))
+s.Queen([0, 1, 2, 3, 4, 5, 6, 7])
