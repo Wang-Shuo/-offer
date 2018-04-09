8134fa371b2ba757fa25ec0f770fc1712d9c73ab
max <wangshuo216@outlook.com>
2018-04-09 19:33:33 +0800

    add task 42 solution2

--- 042.py
+++ 042.py
@@ -18,8 +18,22 @@ class Solution:
 
         return maxSum
 
+class Solution2:
+    def FindGreatestSumOfSubArray(self, numbers):
+        if len(numbers) <= 0 or not numbers:
+            return
+
+        sumList = [0] * len(numbers)
+        for i in range(len(numbers)):
+            if i == 0 or sumList[i-1] <= 0:
+                sumList[i] = numbers[i]
+            else:
+                sumList[i] = sumList[i-1] + numbers[i]
+        
+        return max(sumList)
+
 # test
-s = Solution()
+s = Solution2()
 print(s.FindGreatestSumOfSubArray([1, -2, 3, 10, -4, 7, 2, -5]))
 print(s.FindGreatestSumOfSubArray([1, 2, -3, 4, 5]))
 print(s.FindGreatestSumOfSubArray([]))
