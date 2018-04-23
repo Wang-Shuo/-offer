59b1cbeab023aadc688220f5e466b5f92e353c4b
max <wangshuo216@outlook.com>
2018-04-23 22:15:06 +0800

    update task 55

--- 055.py
+++ 055.py
@@ -18,6 +18,36 @@ class Solution:
         else:
             return nRight + 1
 
+    def IsBalanced(self, pRoot):
+        if not pRoot:
+            return True
+        
+        left = self.TreeDepth(pRoot.left)
+        right = self.TreeDepth(pRoot.right)
+        diff = left - right
+        if diff > 1 or diff < -1:
+            return False
+
+        return self.IsBalanced(pRoot.left) and self.IsBalanced(pRoot.right)
+
+
+class Solution2:
+    def IsBalanced(self, pRoot):
+        return self._is_balanced(pRoot)[0]
+
+    def _is_balanced(self, pRoot):
+        if not pRoot:
+            return (True, 0)
+
+        left = self._is_balanced(pRoot.left)
+        right = self._is_balanced(pRoot.right)
+        if left[0] and right[0]:
+            diff = left[1] - right[1]
+            if abs(diff) <= 1:
+                return (True, max(left[1], right[1]) + 1)
+
+        return (False, max(left[1], right[1]) + 1)
+
 
 # test 
 pRoot = TreeNode(1)
@@ -37,3 +67,6 @@ node4.left = node6
 
 s = Solution()
 print(s.TreeDepth(pRoot))
+print(s.IsBalanced(pRoot))
+s2 = Solution2()
+print(s2.IsBalanced(pRoot))
