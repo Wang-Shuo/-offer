89da7472cd2fecf873d5b5e405e4edef100b97d6
max <wangshuo216@outlook.com>
2018-04-24 09:40:38 +0800

    update task 56

--- 056.py
+++ 056.py
@@ -31,6 +31,28 @@ class Solution:
         return (num & 1)
 
 
+    def FindNumberAppearingOnce(self, numbers):
+        if not numbers or len(numbers) <= 3:
+            return
+        bitSum = [0] * 32
+        for num in numbers:
+            bitMask = 1
+            for i in range(31, -1, -1):
+                bit = num & bitMask
+                if bit != 0:
+                    bitSum[i] += 1
+                bitMask = bitMask << 1
+
+        result = 0
+        for j in range(32):
+            result = result << 1
+            result += bitSum[j] % 3
+
+        return result
+
+
+
 # test 
 s = Solution()
 print(s.FindNumsAppearOnce([2, 4, 3, 6, 3, 2, 5, 5]))
+print(s.FindNumberAppearingOnce([3, 1, 3, 3, 2, 2, 2]))
