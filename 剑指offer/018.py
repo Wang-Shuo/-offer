56f925c1fef1b5eca23b607393841da414b04f2f
max <wangshuo216@outlook.com>
2018-06-06 22:09:38 +0800

    add linked list item cycle

--- 剑指offer/018.py
+++ 剑指offer/018.py
@@ -60,38 +60,38 @@ class Solution:
 
 
 # test
-# node1 = ListNode(1)
-# node2 = ListNode(2)
-# node3 = ListNode(3)
-# node4 = ListNode(4)
-# node1.next = node2
-# node2.next = node3
-# node3.next = node4
-#
-# s = Solution()
-# s.DeleteNode(node1, node2)
-# print(node2.val)
-# s.DeleteNode(node1, node4)
-# print(node4.val)
-# s.DeleteNode(node1, node1)
-# print(node1.val)
-
-s = Solution()
 node1 = ListNode(1)
 node2 = ListNode(2)
-node3 = ListNode(2)
-node4 = ListNode(3)
-node5 = ListNode(3)
-node6 = ListNode(4)
-node7 = ListNode(5)
+node3 = ListNode(3)
+node4 = ListNode(4)
 node1.next = node2
 node2.next = node3
 node3.next = node4
-node4.next = node5
-node5.next = node6
-node6.next = node7
 
-pHead = s.DeleteDuplication(node1)
-while pHead:
-    print(pHead.val)
-    pHead = pHead.next
+s = Solution()
+s.DeleteNode(node1, node2)
+print(node2.val)
+s.DeleteNode(node1, node4)
+print(node4.val)
+s.DeleteNode(node1, node1)
+print(node1.val)
+
+# s = Solution()
+# node1 = ListNode(1)
+# node2 = ListNode(2)
+# node3 = ListNode(2)
+# node4 = ListNode(3)
+# node5 = ListNode(3)
+# node6 = ListNode(4)
+# node7 = ListNode(5)
+# node1.next = node2
+# node2.next = node3
+# node3.next = node4
+# node4.next = node5
+# node5.next = node6
+# node6.next = node7
+
+# pHead = s.DeleteDuplication(node1)
+# while pHead:
+#     print(pHead.val)
+#     pHead = pHead.next
